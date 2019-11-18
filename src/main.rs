48cdaca0b220aa66b57ce36a6a4e6c8b9bf0adbf
ekuinox <depkey@me.com>
2019-11-18 23:59:08 +0900

    fmt

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-use std::env;
 use dotenv::dotenv;
 use regex::Regex;
 use serenity::{
     model::{channel::Message, gateway::Ready, guild::Guild},
     prelude::*,
 };
+use std::env;
 
 struct Handler;
 
@@ -14,22 +14,25 @@ impl EventHandler for Handler {
         if let Some(caps) = re.captures(msg.content.as_str()) {
             let app_id = &caps[1];
 
-            if let Err(why) = msg.channel_id.say(&ctx.http, format!("steam://store/{}", app_id)) {
+            if let Err(why) = msg
+                .channel_id
+                .say(&ctx.http, format!("steam://store/{}", app_id))
+            {
                 println!("Error sending message: {:?}", why);
             }
         }
     }
 
     fn ready(&self, ctx: Context, ready: Ready) {
-        ready.guilds.into_iter()
-            .map(move |guild| { guild.id() })
-            .map(move |id| {
-                match Guild::get(&ctx.http, id) {
-                    Ok(guild) => Some((id, guild.name)),
-                    Err(_) => None
-                }
+        ready
+            .guilds
+            .into_iter()
+            .map(move |guild| guild.id())
+            .map(move |id| match Guild::get(&ctx.http, id) {
+                Ok(guild) => Some((id, guild.name)),
+                Err(_) => None,
             })
-            .filter_map(move |r| { r })
+            .filter_map(move |r| r)
             .for_each(move |(id, name)| {
                 println!("joined at {} ({})", name, id);
             });
@@ -40,8 +43,7 @@ impl EventHandler for Handler {
 fn main() {
     dotenv().ok();
 
-    let token = env::var("DISCORD_TOKEN")
-        .expect("Expected a token in the environment");
+    let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
 
     let mut client = Client::new(&token, Handler).expect("Err creating client");
 
